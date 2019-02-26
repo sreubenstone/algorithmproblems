5cd3ea183a21092cccbbcf37f79abbcb538f37a9
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2019-02-26 15:46:38 -0500

    new one

---
+++ oddmanout.js
@@ -0,0 +1,15 @@
+function iqTest(numbers) {
+
+
+
+}
+
+
+
+/*
+--create array of numbers from string
+--loop through this array and tabulate to variables like even, odd and increment them for each item
+--determine if the odd or even one is larger
+--go back into array and find the even / odd number
+-- return the position in "human terms"
+*/
