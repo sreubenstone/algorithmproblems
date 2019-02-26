a8e0b33140b06bac724aa3167cee765dfda399b4
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2019-02-26 16:14:39 -0500

    pushing

---
+++ toCamel.js
@@ -0,0 +1,29 @@
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+The first word within the output should be capitalized only if the original word was capitalized.
+
+**EXAMPLES**
+
+toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+*/
+
+function toCamelCase(str) {
+
+
+
+
+
+}
+
+
+
+
+/*
+MY LOGIC
+
+-- split each word into an array delimited by -  e.g., ---> ["the", "stealth", "warrior"]
+-- loop through array and modify each index's string to capitalize it starting at position 1
+-- join the words together i think this would work
+
+*/
